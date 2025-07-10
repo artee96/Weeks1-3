9d2a714111bcb4d4c28ad0972d7f6cf177d8f9c9
artee96 <evelybe@sheridancollege.ca>
2025-07-10 15:35:14 -0400

    Made the chaser change colour at different positions

--- Assets/Scripts/Chaser.cs
+++ Assets/Scripts/Chaser.cs
@@ -7,6 +7,8 @@ public class Chaser : MonoBehaviour
     public Vector3 newPosition;
     public Camera gameCamera;
     public float speed;
+    Vector3 lastClickedPosition = Vector3.zero;
+    public SpriteRenderer chaserRenderer;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +21,37 @@ public class Chaser : MonoBehaviour
     {
         Vector3 mousePositionInWorldSpace = gameCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePositionInWorldSpace.z = 0f;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPosition = mousePositionInWorldSpace;
+        }
+
         Vector3 start = transform.position;
-        Vector3 target = mousePositionInWorldSpace;
+        Vector3 target = lastClickedPosition;
         Vector3 directionToMove = target - start;
 
         transform.position = transform.position + directionToMove * speed;
+
+        Vector3 chaserPositionInScreenSpace = gameCamera.WorldToScreenPoint(transform.position);
+        float xMin = 0f;
+        float yMin = 0f;
+        float xMax = Screen.width;
+        float yMax = Screen.height;
+
+        bool xMaxExceeded = chaserPositionInScreenSpace.x >= xMax;
+        bool xMinExceeded = chaserPositionInScreenSpace.x <= xMin;
+
+        Debug.Log("xMaxExceeded =" + xMaxExceeded.ToString());
+        Debug.Log("xMinExceeded =" + xMinExceeded.ToString());
+
+        if (xMaxExceeded || xMinExceeded)
+        {
+            chaserRenderer.color = Color.red;
+        }
+        else
+        {
+            chaserRenderer.color = Color.white;
+        }
     }
 }
