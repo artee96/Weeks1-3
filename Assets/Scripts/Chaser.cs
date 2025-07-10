7bb004a218cf896d0c94a9f29319df619fd1b1cd
artee96 <evelybe@sheridancollege.ca>
2025-07-10 15:18:56 -0400

    created chaser script and game object to appear at click

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+    public Vector3 newPosition;
+    public Camera gameCamera;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePositionInWorldSpace = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePositionInWorldSpace.z = 0f;
+        Vector3 start = transform.position;
+        Vector3 target = mousePositionInWorldSpace;
+        Vector3 directionToMove = target - start;
+
+        transform.position = transform.position + directionToMove * speed;
+    }
+}
